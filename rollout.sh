5042a7b7696d3332ba546968fff1e2c5b5ec1943
hanxianchao <12164595+hanxianchao66@user.noreply.gitee.com>
2024-11-18 14:24:39 +0000

    update rollout.sh.
    Signed-off-by: hanxianchao <12164595+hanxianchao66@user.noreply.gitee.com>

--- rollout.sh
+++ rollout.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# 检查输入参数
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <action> <deployment> [namespace]"
     echo "Actions: undo, history"
-    echo "Example: $0 undo jenkins-demo production"
     exit 1
 fi
 
-# 参数解析
-ACTION=$1                # 操作类型，如 undo 或 history
-DEPLOYMENT=$2            # 部署名称
-NAMESPACE=${3:-default}  # 命名空间，默认为 default
+ACTION=$1
+DEPLOYMENT=$2
+NAMESPACE=${3:-default}
 
-# 执行操作
 case $ACTION in
     undo)
         echo "Rolling back deployment $DEPLOYMENT in namespace $NAMESPACE..."
@@ -25,7 +21,6 @@ case $ACTION in
         ;;
     *)
         echo "Invalid action: $ACTION"
-        echo "Valid actions: undo, history"
         exit 1
         ;;
 esac
